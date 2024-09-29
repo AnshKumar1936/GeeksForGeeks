0ab58aad8cdf4cd4e71170af6e38d5b5edfc556a
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-09-29 22:18:26 +0530

    Create   Total count.java

---
+++ Easy/Total count.java
@@ -0,0 +1,15 @@
+class Solution {
+    int totalCount(int k, int[] arr) {
+        // code here
+        int count= 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%k == 0){
+                count+= arr[i]/k;
+            }
+            else{
+                count+=arr[i]/k + 1;
+            }
+        }
+        return count;
+    }
+}
