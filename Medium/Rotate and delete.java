6919f071b11b4f9fde146cfb6f53bbd34fcfe39a
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-10-02 18:30:55 +0530

    Create Rotate and delete.java

---
+++ Medium/Rotate and delete.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static int rotateDelete(ArrayList<Integer> arr) {
+        int n=arr.size(),temp=n/2;
+        int i=3*(temp/2);
+        if(temp%2==1) i++;
+        return arr.get(n-i);
+    }
+}
